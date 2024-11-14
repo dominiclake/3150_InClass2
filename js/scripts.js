d7e7a430de5c50544154b2979c054a72a3947467
dominiclake <144931780+dominiclake@users.noreply.github.com>
2024-11-14 14:02:25 -0500

    main

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function avis(){
+    document.getElementById("a-picture").style.visibility = 'visible';
+}
+
+function bvis(){
+    document.getElementById("b-picture").style.visibility = 'visible';
+}
+
+function cvis(){
+    document.getElementById("c-picture").style.visibility = 'visible';
+}
+
+function ainvis(){
+    document.getElementById("a-picture").style.visibility = 'hidden';
+}
+
+function binvis(){
+    document.getElementById("b-picture").style.visibility = 'hidden';
+}
+
+function cinvis(){
+    document.getElementById("c-picture").style.visibility = 'hidden';
+}
